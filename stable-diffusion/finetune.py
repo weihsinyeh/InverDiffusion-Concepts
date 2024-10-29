17dd5653d1519ed18634b38fdeeed617fee47c7f
Wei-Hsin Yeh <weihsinyeh168@gmail.com>
2024-10-29 14:28:41 +0800

    Fix path of output checkpoint

--- stable-diffusion/finetune.py
+++ stable-diffusion/finetune.py
@@ -198,7 +198,7 @@ class TextualInversionDataset(Dataset):
 
 config_path = "./configs/stable-diffusion/v1-inference.yaml"
 checkpoint_path = "./ldm/models/stable-diffusion-v1/model.ckpt"
-output_dir = "/project/g/r13922043/hw2/checkpoints/1029_data_aug"
+output_dir = "/project/g/r13922043/hw2/checkpoints/1029_0"
 image_folder = "/project/g/r13922043/hw2_data/textual_inversion/0"
 placeholder_token = "<new1>"
 
